fc41a7a5460782194039c02946cd3721ce30298f
eric0989 <eric0989@hotmail.com>
2016-04-26 19:03:42 +0800

    HW_1050426

--- HW_1050426.cpp
+++ HW_1050426.cpp
@@ -8,22 +8,58 @@
 #include <stdlib.h>
 
 int main(){
-  int **A, **B, **C;
-  int *data;
-  int N;
+  double **A, **B, **C;
+  double *rowA, *rowB, *tem;
+  int i, j, k, N;
 
   printf("Enter N = ");
   scanf("%d",&N);
-  A = (int **)malloc(N*sizeof(int *));
-  B = (int **)malloc(N*sizeof(int *));
-  data = (int*)malloc(N*N*sizeof(int));
-  for(int i=0;i<N;i++){
-    data[i] = 1;
-    A[i] = data;
+  A = (double **)malloc(N*sizeof(double *));
+  B = (double **)malloc(N*sizeof(double *));
+  C = (double **)malloc(N*sizeof(double *));
+  rowA = (double *)malloc(N*N*sizeof(double));
+  rowB = (double *)malloc(N*N*sizeof(double));
+  tem = (double *)malloc(N*N*sizeof(double));
+  for(i=0;i<N;i++){
+    rowA[i] = i;
+    A[i] = rowA;
   }
-  for(int i=0;i<N;i++){
-    for(int j=0;j<N;j++){
-      printf("%d",A[i][j]);
+  for(i=0;i<N;i++){
+    rowB[i] = 1;
+    B[i] = rowB;
+  }
+  for(i=0;i<N;i++){
+    tem[i] = 0;
+    C[i] = tem;
+  }
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      for(k=0;k<N;k++){
+        C[i][j] = C[i][j]+A[i][k]*B[k][j];
+      }
+    }
+  }
+
+  printf("A =\n");
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%f  ",A[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("B =\n");
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%f  ",B[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("A*B =\n");
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%f  ",C[i][j]);
     }
     printf("\n");
   }
