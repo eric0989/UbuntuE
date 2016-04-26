5486f9a1f635d1350ef621a12a3f47e1245a44c6
eric0989 <eric0989@hotmail.com>
2016-04-26 16:10:44 +0800

    HW_1050426

--- HW_1050426.cpp
+++ HW_1050426.cpp
@@ -8,20 +8,25 @@
 #include <stdlib.h>
 
 int main(){
-  int **A, **B, **C, *data;
+  int **A, **B, **C;
+  int *data;
   int N;
 
   printf("Enter N = ");
   scanf("%d",&N);
   A = (int **)malloc(N*sizeof(int *));
+  B = (int **)malloc(N*sizeof(int *));
   data = (int*)malloc(N*N*sizeof(int));
-  for(int i=0;i<N;i++,data+=N){
+  for(int i=0;i<N;i++){
+    data[i] = 1;
     A[i] = data;
   }
   for(int i=0;i<N;i++){
-  printf("%d",A[i]);
+    for(int j=0;j<N;j++){
+      printf("%d",A[i][j]);
+    }
+    printf("\n");
   }
-
 
   return 0;
 }
--- test01.cpp
+++ test01.cpp
@@ -1,6 +1,16 @@
 # include <stdio.h>
+# include <math.h>
+# include <stdlib.h>
 
 int main(){
-  printf("Test!\n");
+  int A[3][3] = {{0,1,2},{3,4,5},{6,7,8}};
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      printf("%d  ",A[i][j]);
+    }
+  printf("\n");
+  }
+
+
   return 0;
 }
