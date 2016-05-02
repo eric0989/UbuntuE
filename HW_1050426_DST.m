ea85f57e8c8582bdf18ac3657e6501b282c6b3d9
eric0989 <eric0989@hotmail.com>
2016-05-02 22:39:51 +0800

    HW_DST

---
+++ HW_1050426_DST.m
@@ -0,0 +1,23 @@
+N = input("Please enter N = ");
+
+for i = 1:N
+  X(i) = i-1;
+endfor
+
+X
+Z(1) = 0;
+Z(N+2) = 0;
+
+for i = 2:N+1
+  Z(i) = X(i-1);
+endfor
+
+for i = N+3:2*N+2
+  Z(i) = -X(2*N+3-i);
+endfor
+
+Z
+A = imag((fft(Z))/(-2));
+S_X = A(2:N+1)
+B = imag((fft(A))/(-2));
+SS_X = B(2:N+1)*(2/(N+1))
--- test.m
+++
@@ -1,6 +0,0 @@
-A = [1 2 3;4 5 6;7 8 9]
-B = [1 2 3;4 5 6;7 8 9]
-C = A*B
-normA = norm(A)
-normB = norm(B,'fro')
-normB^2
