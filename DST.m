561fad4d774ac12c0ceae0ee0c87662103db6e93
eric0989 <eric0989@hotmail.com>
2016-05-03 14:14:33 +0800

    DST

---
+++ DST.m
@@ -0,0 +1,18 @@
+function [S_X] = DST(N)
+
+for i = 1:N
+  X(i) = i-1;
+endfor
+
+Z(1) = 0;
+Z(N+2) = 0;
+
+for i = 2:N+1
+  Z(i) = X(i-1);
+endfor
+for i = N+3:2*N+2
+  Z(i) = -X(2*N+3-i);
+endfor
+
+A = imag((fft(Z))/(-2));
+S_X = A(2:N+1);
