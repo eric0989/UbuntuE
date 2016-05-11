1f93d7da5e8e8b3969e82ff36847c3241ea7e9fd
eric0989 <eric0989@hotmail.com>
2016-05-11 20:00:24 +0800

    err function

--- DST.cpp
+++ DST.cpp
@@ -14,32 +14,36 @@ void initial(fftw_complex *, int);
 void extend(fftw_complex *, int);
 void fft(fftw_complex *, fftw_complex *, int);
 void print_complex_vector(fftw_complex *, int);
+void print_vector(fftw_complex *, int);
+double err(fftw_complex *, fftw_complex *, int);
 
 int main(){
 	int i, N;
-	fftw_complex *in, *out;
+	fftw_complex *A, *in, *out;
 	double t1, t2;
 
 	t1 = clock();
 
 	printf("\n Enter N = ");
 	scanf("%d",&N);
+	A = (fftw_complex *)fftw_malloc((2*N+2)*sizeof(fftw_complex));
 	in = (fftw_complex *)fftw_malloc((2*N+2)*sizeof(fftw_complex));
 	out = (fftw_complex *)fftw_malloc((2*N+2)*sizeof(fftw_complex));
 
-	for(i=0;i<N;i++)
-		in[i][0] = i*1.0;
+	for(i=0;i<N;i++)	// input value in matrix A
+		A[i][0] = i*1.0;
+	for(i=0;i<N;i++)	// copy the value of A
+		in[i][0] = A[i][0];
 	dst(in, out, N);
 	printf("\n DST out :\n");
-	for(i=0;i<N;i++)
-		printf(" [%d] %f\n",i,in[i][0]);
+	print_vector(in, N);
 
-	dst(in, out, N);
-	printf("\n iDST out :\n");
-	for(i=0;i<N;i++){
+	dst(in, out, N);	// DST again
+	for(i=0;i<N;i++)	// iDST scalar
 		in[i][0] = in[i][0]*(2.0/(N+1)); //N is int
-		printf(" [%d] %f\n",i,in[i][0]);
-	}
+	printf("\n iDST out :\n");
+	print_vector(in, N);
+	printf("\n error = %e\n",err(A, in, N));
 
 	t2 = clock();
 	printf("\n Time = %f s\n\n",(t2-t1)/CLOCKS_PER_SEC);
@@ -108,4 +112,23 @@ void print_complex_vector(fftw_complex *x, int N){
 	}
 
 	return;
+}
+
+void print_vector(fftw_complex *x, int N){
+	int i;
+
+	for(i=0;i<N;i++)
+                printf(" [%d] %f\n",i,x[i][0]);
+
+	return;
+}
+
+double err(fftw_complex *x, fftw_complex *y, int N){
+	int i;
+	double e = 0.0;
+
+	for(i=0;i<N;i++)
+		if(fabs(x[i][0]-y[i][0])>e)	e = abs(x[i][0]-y[i][0]);
+
+	return e;
 }
