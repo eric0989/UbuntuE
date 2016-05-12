a45464966b8d5e83814e2561fac65ccc9d30db46
eric0989 <eric0989@hotmail.com>
2016-05-12 19:26:48 +0800

    0511

--- 0511.cpp
+++ 0511.cpp
@@ -9,12 +9,15 @@
 #include <time.h>
 #include <cblas.h>
 
+void MxM(double *, double *, double *, int);
 void dgemm_row(double *, double *, double *, int);
+double err(double *, double *, int);
+void print_matrix(double *, int);
 
 int main(){
 	double *A, *B, *C, *D;
-	int i, j, k, N;
-	double t1, t2, t3, t4, err=0.0;
+	int i, N;
+	double t1, t2, t3, t4;
 
 	printf("\n Enter N = ");
 	scanf("%d",&N);
@@ -23,37 +26,42 @@ int main(){
 	C = (double *)malloc(N*N*sizeof(double));
 	D = (double *)malloc(N*N*sizeof(double));
 
-	for(i=0;i<N*N;i++){
+	for(i=0;i<N*N;i++){	// input A B
 		A[i] = i+1;
 		B[i] = N*N-i;
-		C[i] = 0;
 	}
 
 	t1 = clock();
+	MxM(A, B, C, N);
+	t2 = clock();
+
+	t3 = clock();
+	dgemm_row(A, B, D, N);
+	t4 = clock();
+
+	printf("\n Time = %f s\n",(t2-t1)/CLOCKS_PER_SEC);
+	printf(" Time_blas = %f s\n",(t4-t3)/CLOCKS_PER_SEC);
+	printf(" error = %e\n\n",err(C, D, N));
+/*
+	printf(" C =\n");
+	print_matrix(C, N);
+	printf(" D =\n");
+	print_matrix(D, N);
+*/
+	return 0;
+}
+
+void MxM(double *x, double *y, double *z, int N){
+	int i, j, k;
 
 	for(i=0;i<N;i++){
 		for(j=0;j<N;j++){
 			for(k=0;k<N;k++)
-				C[N*i+j] = C[N*i+j]+A[N*i+k]*B[j+N*k];
+				z[N*i+j] = z[N*i+j]+x[N*i+k]*y[j+N*k];
 		}
 	}
 
-	t2 = clock();
-
-	t3 = clock();
-
-	dgemm_row(A, B, D, N);
-
-	t4 = clock();
-
-	for(i=0;i<N*N;i++)
-		if(fabs(D[i]-C[i])>err)	err = fabs(D[i]-C[i]);
-
-	printf("\n Time = %f s\n",(t2-t1)/CLOCKS_PER_SEC);
-	printf(" Time_blas = %f s\n",(t4-t3)/CLOCKS_PER_SEC);
-	printf(" err = %e\n\n",err);
-
-	return 0;
+	return;
 }
 
 void dgemm_row(double *A, double *B, double *C, int N){
@@ -62,6 +70,28 @@ void dgemm_row(double *A, double *B, double *C, int N){
 	beta = 0.0;
 
 	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, alpha, A, N, B, N, beta, C, N);
+
+	return;
+}
+
+double err(double *x, double *y, int N){
+	int i;
+	double e = 0.0;
+
+	for(i=0;i<N*N;i++){
+		if(fabs(x[i]-y[i])>e)	e = fabs(x[i]-y[i]);
+	}
+
+	return e;
+}
+
+void print_matrix(double *x, int N){
+	int i;
+
+	for(i=0;i<N*N;i++){
+		printf(" %.4f  ",x[i]);
+		if((i+1)%N==0)	printf("\n");
+	}
 
 	return;
 }
