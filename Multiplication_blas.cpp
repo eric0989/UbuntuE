742e8424616595183b3fc2b817cdc996e4df82e9
eric0989 <eric0989@hotmail.com>
2016-05-24 19:23:13 +0800

    0511=>blas

--- Multiplication_blas.cpp
+++ Multiplication_blas.cpp
@@ -1,5 +1,5 @@
 /************************************************************
-* Title:  Matrix Multiplication with Single Pointer         *
+* Title:  Matrix Multiplication with blas                   *
 * Date:   2016/05/11                                        *
 ************************************************************/
 
