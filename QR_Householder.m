1ea686e7fca2ab974bcf63f880d83c2f26f26c3c
eric0989 <eric0989@hotmail.com>
2016-05-25 20:55:57 +0800

    complete

--- QR_Householder.m
+++ QR_Householder.m
@@ -1,4 +1,4 @@
-N = 4;
+N = 3;
 
 for i = 1:N
 	for j = 1:N
@@ -16,10 +16,16 @@ for k = 1:N
 	v = sign(x(1))*norm(x)*e + x;
 	v = v/norm(v);
 	R(k:N, k:N) = R(k:N, k:N) - 2*v*(v'*R(k:N, k:N));
-	Q(k:N, k:N) = Q(k:N, k:N) - 2*v*(v'*Q(k:N, k:N));
+	V(k:N, k) = v;
+%	Q(k:N, k:N) = Q(k:N, k:N) - 2*v*(v'*Q(k:N, k:N));
 end
+
+for k = N:-1:1
+	Q(k:N, k:N) = Q(k:N, k:N) - 2*V(k:N, k)*(V(k:N, k)'*Q(k:N, k:N));
+end
+
 A
-Q = Q'
+Q
 R
 QR = Q*R
 err = max(max(abs(QR-A)))
